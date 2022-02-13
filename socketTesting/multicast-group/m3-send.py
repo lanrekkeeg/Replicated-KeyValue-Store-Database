9010be3dd0b49ca94501ca664d73a5b0fbe5b6cc
lanrekkeeg <p156058@nu.edu.pk>
2022-02-13 17:46:16 +0100

    test cases

---
+++ socketTesting/multicast-group/m3-send.py
@@ -0,0 +1,14 @@
+import socket
+
+def send(data, port=50000, addr='239.192.1.100'):
+        """send(data[, port[, addr]]) - multicasts a UDP datagram."""
+        # Create the socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # Make the socket multicast-aware, and set TTL.
+        s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 20) # Change TTL (=20) to suit
+        # Send the data
+        data = b"hello world"
+        while True:
+                s.sendto(data, (addr, port))
+
+send()
--- socketTesting/multicast-group/send2.py
+++ socketTesting/multicast-group/send2.py
@@ -2,4 +2,4 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 3)
-sock.sendto(b"robot", ("239.192.0.100", 1000))
+sock.sendto(b"robot", ("224.1.1.1", 1000))
